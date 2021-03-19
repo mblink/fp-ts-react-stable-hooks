feca0d48801973f45a0f30b2719f6b3917e75183
Harim Tejada <harim@bondlink.com>
2021-03-19 11:42:58 -0400

    Improve useStableCallback tests
    Co-authored-by: Justin Leider <justin@bondlink.com>

--- test/callback.test.ts
+++ test/callback.test.ts
@@ -11,18 +11,20 @@ const nEq = Eq.getTupleEq(O.getEq(Eq.eqNumber));
 describe('useStableCallback', () => {
   test('should not return a new function if the values are the same', () => {
     let o = o1a;
-    const { result, rerender, } = renderHook(() => useStableCallback(() => o, [o], nEq));
+    const { result, rerender } = renderHook(() => useStableCallback(() => o, [o], nEq));
     o = o1b;
     rerender();
+    expect(result.all[0]).toStrictEqual(result.all[1]);
     expect(result.current()).toStrictEqual(o1a);
   });
 
   test('should return a new function if the values are different', () => {
     let o = o1a;
-    const { result, rerender, } = renderHook(() => useStableCallback((a: number) => O.getOrElse(() => 0)(o) + a, [o], nEq));
+    const { result, rerender } = renderHook(() => useStableCallback((a: number) => O.getOrElse(() => 0)(o) + a, [o], nEq));
     expect(result.current(1)).toStrictEqual(2);
     o = o2;
     rerender();
+    expect(result.all[0]).not.toStrictEqual(result.all[1]);
     expect(result.current(1)).toStrictEqual(3);
   });
 });
